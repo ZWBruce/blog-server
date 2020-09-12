aca914f859caf76f86a6b4d42b4f365fb3a935c3
张伟 <zhangweibe@chanjet.com>
2020-09-13 01:40:14 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,26 @@
+const Koa = require('koa')
+const Router = require('koa-router');
+const process = require('shelljs')
+
+const app = new Koa()
+const router = new Router();
+
+
+router.get('/', (ctx) => {
+  ctx.body = 'user的首页' + __dirname;
+});
+
+
+router.get('/update', (ctx) => {
+  try {
+    process.exec('sudo cd /usr/local/myBlog && git fetch --all && git reset --hard origin/master && npm run go')
+    ctx.body = 'update success'
+  } catch (e) {
+    ctx.body = e
+  }
+})
+
+app.use(router.routes())
+app.listen(8080)
+
+// console.log('env: ', process.env.NODE_ENV)
